fe064625d4f072cd35b2a927cf3a61f8a64f84b4
Shaun Reynolds <tdcharactercreator>
2020-09-29 20:40:02 -0400

    xcv

---
+++ gui/app/common/polySelector/desktop/polySelectorDesktopDir-@{TDCC_VERSION}.js
@@ -0,0 +1,35 @@
+angular.module('main').directive('polySelectorDesktop',['$uibModal', function($uibModal){
+    return {
+        restrict:'E',
+        scope:{
+            characterContext:'=',
+            elementId: '@',
+            setPoly: '&?'
+        },
+        link: function(scope) {
+            scope.modalInstance = null;
+            
+            scope.openModal = function() {
+                scope.modalInstance = $uibModal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    bindToController: true,
+                    scope: scope,
+                    windowClass: 'desktop-vtd-dialog',
+                    openedClass: 'desktop-modal-content',
+                    templateUrl: 'common/polySelector/desktop/polySelectorDesktopModalTemplate-@{TDCC_VERSION}.html'
+                }); 
+            };
+            
+            scope.selectPoly = function(poly) {
+                scope.setPoly()(poly.id); 
+                scope.modalInstance.close();
+            }; 
+            
+            scope.closeModal = function() {
+                scope.modalInstance.close();
+            };
+        },
+        templateUrl:'common/polySelector/desktop/polySelectorDesktopTemplate-@{TDCC_VERSION}.html'
+    };
+}]);
